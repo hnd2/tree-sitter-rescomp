bda7462de5cc3732903aa3da6301529e443bb315
hnd <honda@softdevice.co.jp>
2025-07-25 18:18:21 +0900

    add queries

---
+++ queries/highlights.scm
@@ -0,0 +1,18 @@
+; keyword
+[
+    "PALETTE"
+    "BITMAP"
+    "TILESET"
+    "TILEMAP"
+    "MAP"
+    "OBJECTS"
+    "IMAGE"
+    "SPRITE"
+    "XGM"
+    "XGM2"
+    "WAV"
+    "BIN"
+    "ALIGN"
+    "UNGROUP"
+    "NEAR"
+] @keyword
